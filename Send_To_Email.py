868466906aaf12a8c7312fa94786c83e5d619d18
Rishav Nandi <rishavnandi2@gmail.com>
2020-04-24 18:49:07 +0530

    Formatted Code With Black For Ease Of Readability

--- Send_To_Email.py
+++ Send_To_Email.py
@@ -9,25 +9,25 @@ toaddr = "RecieverAddress"
 
 msg = MIMEMultipart()
 
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "Add Subject"
+msg["From"] = fromaddr
+msg["To"] = toaddr
+msg["Subject"] = "Add Subject"
 
 body = "Add Body"
 
-msg.attach(MIMEText(body, 'plain'))
+msg.attach(MIMEText(body, "plain"))
 
 filename = "Stock_Data.csv OR Price_Summary.csv"
 attachment = open("/Path/to/.csv", "rb")
 
-part = MIMEBase('application', 'octet-stream')
+part = MIMEBase("application", "octet-stream")
 part.set_payload((attachment).read())
 encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+part.add_header("Content-Disposition", "attachment; filename= %s" % filename)
 
 msg.attach(part)
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
+server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 server.login(fromaddr, "AddYourPassword")
 text = msg.as_string()
--- Stock_Scraper.py
+++ Stock_Scraper.py
@@ -1,31 +1,31 @@
-#Summary Of Data:
+# Summary Of Data:
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd 
+import pandas as pd
 
 labels = []
 data = []
 
-url = 'Add url Of Desired Stock'
+url = "Add url Of Desired Stock"
 
 request = requests.get(url)
 response = request.status_code
 if response == 200:
     pass
 else:
-    print('Server responded with', response)
-soup = BeautifulSoup(request.content,'html.parser')
-table = soup.find('div',id='quote-summary')
-rows = table.find_all('tr')
+    print("Server responded with", response)
+soup = BeautifulSoup(request.content, "html.parser")
+table = soup.find("div", id="quote-summary")
+rows = table.find_all("tr")
 for row in rows:
-    labels.append(str(row.find_all('td')[0].text))
-    data.append(str(row.find_all('td')[1].text))
-cols = {'Field':labels,'Data':data}
+    labels.append(str(row.find_all("td")[0].text))
+    data.append(str(row.find_all("td")[1].text))
+cols = {"Field": labels, "Data": data}
 df = pd.DataFrame(cols)
 
-df.to_csv('Price_Summary.csv')
+df.to_csv("Price_Summary.csv")
 
-#Historical Data:
+# Historical Data:
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -38,28 +38,37 @@ Close = []
 Adj_Close = []
 Volume = []
 
-url = 'Add url Of Desired Stock'
+url = "Add url Of Desired Stock"
 
 request = requests.get(url)
 response = request.status_code
 if response == 200:
     pass
 else:
-    print('Server responded with', response)
-soup = BeautifulSoup(request.content,'html.parser')
-table = soup.find('table',class_='W(100%) M(0)')
-rows = table.find_all('tr',class_='BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)')
+    print("Server responded with", response)
+soup = BeautifulSoup(request.content, "html.parser")
+table = soup.find("table", class_="W(100%) M(0)")
+rows = table.find_all("tr", class_="BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)")
 
 for row in rows:
-    Date.append(str(row.find_all('span')[0].text))
-    Open.append(str(row.find_all('span')[1].text))
-    High.append(str(row.find_all('span')[2].text))
-    Low.append(str(row.find_all('span')[3].text))
-    Close.append(str(row.find_all('span')[4].text))
-    Adj_Close.append(str(row.find_all('span')[5].text))
-    Volume.append(str(row.find_all('span')[6].text))
+    Date.append(str(row.find_all("span")[0].text))
+    Open.append(str(row.find_all("span")[1].text))
+    High.append(str(row.find_all("span")[2].text))
+    Low.append(str(row.find_all("span")[3].text))
+    Close.append(str(row.find_all("span")[4].text))
+    Adj_Close.append(str(row.find_all("span")[5].text))
+    Volume.append(str(row.find_all("span")[6].text))
 
-cols = {'Date':Date,'Open':Open,'High':High,'Low':Low,'Close':Close,'Adj Close':Adj_Close,'Volume':Volume}
+cols = {
+    "Date": Date,
+    "Open": Open,
+    "High": High,
+    "Low": Low,
+    "Close": Close,
+    "Adj Close": Adj_Close,
+    "Volume": Volume,
+}
 df = pd.DataFrame(cols)
 
-df.to_csv('Stock_Data.csv')
+df.to_csv("Stock_Data.csv")
+
